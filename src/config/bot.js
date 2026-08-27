689a56fb599ea15d4a7f1b4f74e53be10138c595
amadihbotz <MILKY5DOG@OUTLOOK.COM>
2026-08-27 15:56:31 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Cinderella", // required by Discord API, not shown in the client
         state: "discord.gg/admm",     // this is what people actually see
         type: 2,               // Custom
       },
